a65ff28bce33fbc2c057c4d21163de62e6605098
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2024-11-17 14:41:42 +0530

    feat: update and download thumbnail

--- src/components/thumbnail-creator.tsx
+++ src/components/thumbnail-creator.tsx
@@ -5,14 +5,56 @@ import Dropzone from "./dropzone";
 import TemplateImage from "./template-image";
 import { useEffect, useRef, useState } from "react";
 import { removeBackground } from "@imgly/background-removal";
+import { Button } from "./ui/button";
+import { IoMdArrowBack } from "react-icons/io";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
+const presets = {
+  style1: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "rgba(255, 255, 255, 1)",
+    opacity: 1,
+  },
+  style2: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "rgba(0, 0, 0, 1)",
+    opacity: 1,
+  },
+  style3: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "rgba(255, 255, 255, 0.8)",
+    opacity: 0.8,
+  },
+};
 
 const ThumbnailCreator = ({}) => {
   const [text, setText] = useState("POV");
-  const [style, setStyle] = useState("style1");
+  const [selectedStyle, setSelectedStyle] = useState("style1");
   const [loading, setLoading] = useState<boolean>(false);
   const [imageSrc, setImageSrc] = useState<string | null>("");
   const [canvasReady, setCanvasReady] = useState<boolean>(false);
   const [processedImageUrl, setProcessedImageUrl] = useState<string | null>("");
+  const [font, setFont] = useState("arial");
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -56,22 +98,33 @@ const ThumbnailCreator = ({}) => {
       canvas.height = image.height;
 
       ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      let preset = presets.style1;
+      switch (selectedStyle) {
+        case "style2":
+          preset = presets.style2;
+          break;
+        case "style3":
+          preset = presets.style3;
+          break;
+      }
+
       ctx.save();
 
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
 
       let fontSize = 100;
-      let selectFont = "Arial";
+      let selectFont = font;
 
-      ctx.font = `${"bold"} ${fontSize}px ${selectFont}`;
+      ctx.font = `${preset.fontWeight} ${fontSize}px ${selectFont}`;
       const textWidth = ctx.measureText(text).width;
       const targetWidth = canvas.width * 0.9;
 
       fontSize *= targetWidth / textWidth;
-      ctx.font = `${"bold"} ${fontSize}px ${selectFont}`;
-      ctx.fillStyle = "rgba(255, 255, 255, 1)";
-      ctx.globalAlpha = 1;
+      ctx.font = `${preset.fontWeight} ${fontSize}px ${selectFont}`;
+      ctx.fillStyle = preset.color;
+      ctx.globalAlpha = preset.opacity;
 
       const x = canvas.width / 2;
       const y = canvas.height / 2;
@@ -90,19 +143,85 @@ const ThumbnailCreator = ({}) => {
     image.src = imageSrc;
   };
 
+  const handleDownload = async () => {
+    if (canvasRef?.current) {
+      const link = document.createElement("a");
+      link.download = "image/png";
+      link.href = canvasRef?.current?.toDataURL();
+      link.click();
+    }
+  };
+
   return (
     <>
       {imageSrc ? (
         <>
           {loading ? (
-            <div className="flex items-center justify-center">
+            <div className="flex h-screen items-center justify-center">
               <div className="h-10 w-10 animate-spin rounded-full border-2 border-dashed border-gray-800"></div>
             </div>
           ) : (
-            <canvas
-              ref={canvasRef}
-              className="max-h-lg h-auto w-full max-w-lg rounded-lg"
-            ></canvas>
+            <div className="flex w-full max-w-2xl flex-col items-center gap-5">
+              <div className="my-4 flex w-full flex-col items-center gap-3">
+                <button
+                  onClick={() => {
+                    setImageSrc(null);
+                    setProcessedImageUrl(null);
+                    setCanvasReady(false);
+                  }}
+                  className="flex items-center gap-2 self-start"
+                >
+                  <IoMdArrowBack className="h-4 w-4" />
+                  <p className="leading-7">Go Back</p>
+                </button>
+                <canvas
+                  ref={canvasRef}
+                  className="max-h-lg h-auto w-full max-w-lg rounded-lg"
+                ></canvas>
+              </div>
+              <Card className="w-full">
+                <CardHeader>
+                  <CardTitle> Edit </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col gap-1.5">
+                      <Label htmlFor="text">Text</Label>
+                      <Input
+                        id="text"
+                        placeholder="text in the thumbnail"
+                        className="h-10 px-3 py-2"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                      />
+                    </div>
+                    <div className="flex flex-col gap-1.5">
+                      <Label htmlFor="font">Font</Label>
+                      <Select
+                        value={font}
+                        onValueChange={(value) => setFont(value)}
+                      >
+                        <SelectTrigger id="font" className="h-10">
+                          <SelectValue placeholder="Select a font" />
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                          <SelectGroup>
+                            <SelectLabel>Fonts</SelectLabel>
+                            <SelectItem value="arial">Arial</SelectItem>
+                            <SelectItem value="inter">Inter</SelectItem>
+                            <SelectItem value="domine">Domine</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </div>
+                </CardContent>
+                <CardFooter className="flex flex-wrap justify-between gap-2">
+                  <Button onClick={() => handleDownload()}>Download</Button>
+                  <Button onClick={drawCompositeImage}>Update</Button>
+                </CardFooter>
+              </Card>
+            </div>
           )}
         </>
       ) : (
@@ -128,23 +247,23 @@ const ThumbnailCreator = ({}) => {
             <TemplateImage
               image="/style1.png"
               selectStyle={() => {
-                setStyle("style1");
+                setSelectedStyle("style1");
               }}
-              isSelected={style === "style1"}
+              isSelected={selectedStyle === "style1"}
             />
             <TemplateImage
-              image="/style1.png"
+              image="/style2.png"
               selectStyle={() => {
-                setStyle("style2");
+                setSelectedStyle("style2");
               }}
-              isSelected={style === "style2"}
+              isSelected={selectedStyle === "style2"}
             />
             <TemplateImage
-              image="/style1.png"
+              image="/style3.png"
               selectStyle={() => {
-                setStyle("style3");
+                setSelectedStyle("style3");
               }}
-              isSelected={style === "style3"}
+              isSelected={selectedStyle === "style3"}
             />
           </div>
           <Dropzone setSelectedImage={setSelectedImage} />
