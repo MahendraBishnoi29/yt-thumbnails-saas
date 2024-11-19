e5e2964d8c9271e8952bcb62577d3747e588c043
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2024-11-19 14:08:24 +0530

    feat: stripe payments

--- src/app/actions/stripe.ts
+++ src/app/actions/stripe.ts
@@ -5,17 +5,19 @@ import { redirect } from "next/navigation";
 import { env } from "~/env";
 import { authOptions } from "~/server/auth";
 import { db } from "~/server/db";
-import { Stripe } from "stripe";
+import Stripe from "stripe";
 
 export const redirectToBillingSession = async (priceId: string) => {
   if (
-    ![env.STRIPE_10_PACK, env.STRIPE_100_PACK, env.STRIPE_25_PACK].includes(
+    ![env.STRIPE_10_PACK, env.STRIPE_25_PACK, env.STRIPE_100_PACK].includes(
       priceId,
     )
   ) {
     throw new Error("Invalid priceId");
   }
+
   const serverSession = await getServerSession(authOptions);
+
   const user = await db.user.findUnique({
     where: {
       id: serverSession?.user.id,
@@ -26,25 +28,19 @@ export const redirectToBillingSession = async (priceId: string) => {
   });
 
   if (!user?.stripeCustomerId) {
-    throw new Error("User does not have a stripe customer id");
+    throw new Error("User has no stripeCustomerId");
   }
 
   const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+
   const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
+    line_items: [{ price: priceId, quantity: 1 }],
     customer: user.stripeCustomerId,
     mode: "payment",
-    success_url: `${env.NEXTAUTH_URL}/api/session-callback`,
+    success_url: `${env.BASE_URL}/api/session-callback`,
   });
 
-  if (!session.url) {
-    throw new Error("Failed to create stripe session");
-  }
+  if (!session.url) throw new Error("No session URL");
 
   redirect(session.url);
 };
--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -1,27 +1,66 @@
-import type Stripe from "stripe";
+import { headers } from "next/headers";
+import Stripe from "stripe";
 import { env } from "~/env";
-import stripe from "stripe";
+import { db } from "~/server/db";
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const signature = req.headers.get("Stripe-Signature");
+  const sig = headers().get("Stripe-Signature");
   const webhookSecret = env.STRIPE_WEBHOOK_SECRET;
-
   let event: Stripe.Event;
 
   try {
-    if (!signature || !webhookSecret) return;
-    event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
   } catch (err) {
-    console.log(`⚠️  Webhook signature verification failed.`, err);
-    return new Response(
-      JSON.stringify({ error: "Webhook signature verification failed" }),
-      {
-        status: 400,
-      },
-    );
+    return new Response(`Webhook Error`, { status: 400 });
   }
-  return new Response(JSON.stringify({ received: true }), {
-    status: 200,
-  });
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      const session = event.data.object;
+      const customerId = session.customer as string;
+
+      const retrivedSession = await stripe.checkout.sessions.retrieve(
+        session.id,
+        { expand: ["line_items"] },
+      );
+
+      const lineItems = retrivedSession.line_items;
+      if (lineItems && lineItems.data.length > 0) {
+        const priceId = lineItems.data[0]?.price?.id ?? undefined;
+
+        // Update user credits based on the priceId
+        if (priceId) {
+          let creditsToAdd = 0;
+
+          // Determine credits to add
+          switch (priceId) {
+            case env.STRIPE_10_PACK:
+              creditsToAdd = 10;
+              break;
+            case env.STRIPE_25_PACK:
+              creditsToAdd = 25;
+              break;
+            case env.STRIPE_100_PACK:
+              creditsToAdd = 100;
+              break;
+          }
+
+          if (creditsToAdd > 0) {
+            await db.user.update({
+              where: { stripeCustomerId: customerId },
+              data: { credits: { increment: creditsToAdd } },
+            });
+          }
+        }
+      }
+      break;
+    default:
+      console.warn("Unhandled event type: " + event.type);
+  }
+
+  return new Response(JSON.stringify({ recieved: true }));
 }
